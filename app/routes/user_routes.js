8f5be05254c735c68591afeb3d20df23b68258a7
kestler01 <kestler.andrew@gmail.com>
2022-03-21 00:25:43 -0400

    Fixed admin detection bug

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -28,42 +28,45 @@ const router = express.Router()
 // SIGN UP
 // POST /sign-up
 router.post('/sign-up', (req, res, next) => {
-	// start a promise chain, so that any errors will pass to `handle`
-	let adminAccess = false
-	// @ts-ignore
-	if (req.body.credentials?.isAdmin === true) { // ignore this linter error here- its works
-		adminAccess = true
-	}
-	Promise.resolve(req.body.credentials)
-		// reject any requests where `credentials.password` is not present, or where
-		// the password is an empty string
-		.then((credentials) => {
-			if (
-				!credentials ||
-				!credentials.password ||
+  // start a promise chain, so that any errors will pass to `handle`
+  let adminAccess = false
+  // console.log(req.body.credentials)
+  // console.log(req.body.credentials.isAdmin)
+
+  if (req.body.credentials.isAdmin) { // ignore this linter error here- its works
+    adminAccess = true
+  }
+  // console.log(adminAccess)
+  Promise.resolve(req.body.credentials)
+  // reject any requests where `credentials.password` is not present, or where
+  // the password is an empty string
+    .then((credentials) => {
+      if (
+        !credentials ||
+			  !credentials.password ||
 				credentials.password !== credentials.password_confirmation
-			) {
-				throw new BadParamsError()
-			}
-		})
-		// generate a hash from the provided password, returning a promise
-		.then(() => bcrypt.hash(req.body.credentials.password, bcryptSaltRounds))
-		.then((hash) => {
-			// return necessary params to create a user
-			return {
-				email: req.body.credentials.email,
-				hashedPassword: hash,
-				isAdmin: adminAccess,
-				isRsvped: false,
-			}
-		})
-		// create user with provided email and hashed password
-		.then((user) => User.create(user))
-		// send the new user object back with status 201, but `hashedPassword`
-		// won't be send because of the `transform` in the User model
-		.then((user) => res.status(201).json({ user: user.toObject() }))
-		// pass any errors along to the error handler
-		.catch(next)
+      ) {
+        throw new BadParamsError()
+      }
+    })
+  // generate a hash from the provided password, returning a promise
+    .then(() => bcrypt.hash(req.body.credentials.password, bcryptSaltRounds))
+    .then((hash) => {
+      // return necessary params to create a user
+      return {
+        email: req.body.credentials.email,
+        hashedPassword: hash,
+        isAdmin: adminAccess,
+        isRsvped: false
+      }
+    })
+  // create user with provided email and hashed password
+    .then((user) => User.create(user))
+  // send the new user object back with status 201, but `hashedPassword`
+  // won't be send because of the `transform` in the User model
+    .then((user) => res.status(201).json({ user: user.toObject() }))
+  // pass any errors along to the error handler
+    .catch(next)
 })
 
 // SIGN IN
